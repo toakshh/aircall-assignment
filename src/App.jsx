9c6ec89b786740ed940c326a096445dc44d9f87e
Aksh <toakshh@gmail.com>
2024-01-03 15:23:53 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/Header/Header.jsx";
+function App() {
+  return (
+    <>
+      <div className="container">
+        <Header />
+        <div className="container-view">Some activities should be here</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
